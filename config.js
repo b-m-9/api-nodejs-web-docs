f424513db0ab7eb6e2415b119b13513cc80c5afb
bogdanmedvedev <www.clsa.ru@gmail.com>
2018-11-02 21:27:29 +0300

    logo

--- config.js
+++ config.js
@@ -2,6 +2,7 @@ module.exports = {
   "baseUrl": "http://localhost:3010",
   "API_URL": "http://localhost:3010/api/v1/",
   "projectName": "API",
+  // "logoBase64": "",
   // "iconBase64": "",
   "hideDeveloper": false
 };
